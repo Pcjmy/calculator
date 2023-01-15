992b9d9d22c53387c2bec5f511155753b14535c1
Pcjmy <1716045191@qq.com>
2023-01-15 15:49:39 +0800

    component

---
+++ src/component/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
